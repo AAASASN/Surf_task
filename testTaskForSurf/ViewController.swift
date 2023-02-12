d628d86841a1c106b276a3f56f716603e89a9b0c
AAASASN <maraenko@gmail.com>
2023-02-12 23:08:37 +0300

    Initial Commit

---
+++ testTaskForSurf/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  testTaskForSurf
+//
+//  Created by Александр Мараенко on 12.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
